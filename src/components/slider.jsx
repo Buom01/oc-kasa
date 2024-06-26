39ba31e054b560837c728e1b739b078989191199
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-06-26 18:42:01 +0200

    Add slider (carrousel)

---
+++ src/components/slider.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import prevIcon from '../assets/previous.svg';
+import nextIcon from '../assets/next.svg';
+
+type sliderImage = {
+    src: String,
+    alt: String
+}
+
+const Slider = ({images}) =>
+{
+    const [position, setPosition] = useState(0);
+
+    const nextAction = () =>
+    {
+        setPosition(
+            (position) => 
+            {
+                if (position + 1 < images.length)
+                    return position + 1;
+                else
+                    return 0;
+            }
+        )
+    };
+    const prevAction = () =>
+    {
+        setPosition(
+            (position) => 
+            {
+                if (position < 1)
+                    return images.length - 1;
+                else
+                    return position - 1;
+            }
+        )
+    }
+
+    const {src, alt} = images[position];
+
+    return (
+        <div className="slider">
+            <img {...{src,alt}} className="image" />
+            <img src={prevIcon} alt="Précédente" className="prev" onClick={prevAction}/>
+            <img src={nextIcon} alt="Suivante" className="next" onClick={nextAction}/>
+            <p className="pagination">{position+1}/{images.length}</p>
+        </div>
+    );
+};
+
+export default Slider;
--- src/routes/logement.jsx
+++ src/routes/logement.jsx
@@ -1,8 +1,16 @@
+import Slider from "../components/slider";
+
+import imageOne from '../assets/eric-muhr-P_XxsdVgtpQ-unsplash.png';
+import imageTwo from '../assets/kalen-emsley-Bkci_8qcdvQ-unsplash.png';
+
 export default function Logement()
 {
   return (
     <>
-      <p>Todo</p>
+      <Slider images={[
+        {src: imageOne, alt: 'Image 1'},
+        {src: imageTwo, alt: 'Image 2'}
+      ]}/>
     </>
   );
 }
