4146ebe9b620782a8810832de3dacf27e3e6cdd9
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-06-27 14:53:28 +0200

    Fix spoiler animation on Chrome

--- src/routes/logement.jsx
+++ src/routes/logement.jsx
@@ -19,7 +19,8 @@ export default function Logement()
       </div>
 
       <Spoiler title="Description">
-        ...
+        ................................<br/>
+        .............................
       </Spoiler>
     </>
   );
