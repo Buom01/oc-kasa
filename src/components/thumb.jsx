743db9575eacb1035e8e3d07f1ec40116859a843
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-07-05 00:13:04 +0200

    Feature: Thumb can accept image, as well

--- src/components/thumb.jsx
+++ src/components/thumb.jsx
@@ -1,8 +1,11 @@
 import styles from './thumb.module.scss'
 
-const Thumb = ({name, ...props}) =>
+const Thumb = ({name, image, ...props}) =>
 (
-    <a {...props} className={styles.main}><span>{name}</span></a>
+    <a {...props} className={styles.main}>
+        <img src={image} alt={name}/>
+        <span>{name}</span>
+    </a>
 )
 
 export default Thumb;
