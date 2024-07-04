4b493859bf3317cb820c0268ea1eda07341a0a9b
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-07-04 22:13:15 +0200

    Conditional overlay opacity

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -2,7 +2,7 @@
 // Header image
 const Introduction = ({title, image}) =>
 (
-    <div className="introduction" style={{background: `center/cover url("${image}"), rgba(0,0,0,0.6)`}}>
+    <div className="introduction" style={{background: `center/cover url("${image}"), rgba(0,0,0,0.${title ? 6 : 3})`}}>
         {title && <h1>{title}</h1>}
     </div>
 );
