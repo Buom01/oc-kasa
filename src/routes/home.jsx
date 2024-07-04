5de9ccbeec189ae3cc549f5d8b83334712bb3d4d
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-07-05 00:15:21 +0200

    Wire mock datas

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -3,17 +3,25 @@ import introBackground from '../assets/eric-muhr-P_XxsdVgtpQ-unsplash.png';
 import ThumbGrid from "../components/thumbgrid";
 import Thumb from "../components/thumb";
 
+import data from '../assets/logements.json';
+
 export default function Home()
 {
   return (
     <>
       <Introduction title="Chez vous, partout et ailleurs" image={introBackground}/>
       <ThumbGrid>
-        <Thumb name="Location 1" href="/lodgement/1"/>
-        <Thumb name="Location 2" href="/lodgement/1"/>
-        <Thumb name="Location 3" href="/lodgement/1"/>
-        <Thumb name="Location 4" href="/lodgement/1"/>
-        <Thumb name="Location 5" href="/lodgement/1"/>
+        {
+          data.map(
+            lodgement =>
+              <Thumb
+                name={lodgement.title}
+                href={`/lodgement/${lodgement.id}`}
+                image={lodgement.cover}
+                key={lodgement.id}
+              />
+          )
+        }
       </ThumbGrid>
     </>
   );
--- src/routes/lodgement.jsx
+++ src/routes/lodgement.jsx
@@ -1,7 +1,8 @@
-import Slider from "../components/slider";
+import { useNavigate, useParams } from "react-router-dom";
 
 import imageOne from '../assets/eric-muhr-P_XxsdVgtpQ-unsplash.png';
 import imageTwo from '../assets/kalen-emsley-Bkci_8qcdvQ-unsplash.png';
+import Slider from "../components/slider";
 import Tags from "../components/tags";
 import Spoiler from "../components/spoiler";
 import Stars from "../components/stars";
@@ -9,36 +10,63 @@ import Equipments from "../components/equipments";
 import Profile from "../components/profile";
 
 import styles from './lodgement.module.scss';
+import data from '../assets/logements.json';
+import { useCallback, useEffect, useState } from "react";
+
+function getLodgementSync(id, from)
+{
+  return data.find(
+    lodgement => lodgement.id === id
+  );
+}
 
 export default function Lodgement()
 {
+  let navigate = useNavigate();
+  let { id } = useParams();
+  const [lodgement, setLogdement] = useState(() => getLodgementSync(id));  // @Présente-moi
+
+  useEffect(
+    () => {
+      const _lodgement = getLodgementSync(id);
+      setLogdement(_lodgement);
+
+      if (!_lodgement)
+        navigate('/404', {replace: true});  // @Présente-moi
+    },
+    [id]
+  );
+
+  let names = lodgement.host.name.split(' '); // @Présente-moi
+  let firstname = names[0];
+  let lastname = names.slice(1).join(' ')
+
   return (
     <div className={styles.main}>
-      <Slider images={[
-        {src: imageOne, alt: 'Image 1'},
-        {src: imageTwo, alt: 'Image 2'}
-      ]}/>
+
+      <Slider images={lodgement.pictures.map(
+        link => ({src: link, alt: lodgement.title})
+      )}/>
+
       <div className={styles.infos}>
-        
-      <div className={styles.infosShowoff}>
-        <header className={styles.header}>
-          <h1>Cozy loft on the Canal Saint-Martin</h1>
-          <p>Paris, île de France</p>
-        </header>
-        <Tags tags={['Cozy', 'Canal']}/>
-      </div>
-      <div className={styles.infosSide}>
-        <Profile firstname={"Alexandre"} lastname={"Dumas"} photo={null}/>
-        <Stars count={3}/>
-      </div>
+        <div className={styles.infosShowoff}>
+          <header className={styles.header}>
+            <h1>{lodgement.title}</h1>
+            <p>{lodgement.location}</p>
+          </header>
+          <Tags tags={lodgement.tags}/>
+        </div>
+        <div className={styles.infosSide}>
+          <Profile firstname={firstname} lastname={lastname} photo={lodgement.host.picture}/>
+          <Stars count={parseInt(lodgement.rating)}/>
+        </div>
       </div>
 
       <div className={styles.details}>
         <Spoiler title="Description">
-          ................................<br/>
-          .............................
+          {lodgement.description}
         </Spoiler>
-        <Equipments values={["Wifi", "Terasse", "..."]}/>
+        <Equipments values={lodgement.equipments}/>
       </div>
     </div>
   );
