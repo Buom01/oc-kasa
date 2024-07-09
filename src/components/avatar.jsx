8acc3ba08d61ae04526cf8ae9ab14bd63abf1d09
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-07-09 21:31:00 +0200

    Fix avatars class

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -2,7 +2,7 @@ import styles from './avatar.module.scss';
 
 const Avatar = ({photo, firstname, lastname}) =>
 (
-    <img src={photo} alt={`${firstname} ${lastname}`} className={styles.avatar}/>
+    <img src={photo} alt={`${firstname} ${lastname}`} className={styles.main}/>
 );
 
 export default Avatar;
